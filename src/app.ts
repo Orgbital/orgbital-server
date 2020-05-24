f35e94d87aeaa6d639537091ded988f1bc25619b
Jing Yen Loh <lohjingyen@gmail.com>
2020-05-25 05:23:42 +0800

    Construct PoC web server that uses json0
    The json0 appears to be a hard dependency of sharedb (c.f.
    https://github.com/share/sharedb/issues/284). To handle plain text
    rather than JSON documents, it appears that we have to register the
    'type' with sharedb.

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,39 @@
 import express from 'express';
+import ShareDb from 'sharedb';
+import http from 'http';
+import { Server as WebSocketServer } from 'ws';
+import WebSocketJSONStream from '@soundation/websocket-json-stream';
 
-const app = express();
+const backend = new ShareDb();
 
-app.get('/', (_, res) => res.send('Hello.'));
+function createDoc (cb: () => void): void {
+  const conn = backend.connect();
+  // Hardcode a doc for PoC purposes
+  const doc = conn.get('foo', 'bar');
+  doc.fetch(err => {
+    if (err) throw err;
 
-app.listen(3000);
+    if (doc.type === null) {
+      doc.create({ content: "public class Program {}" }, cb);
+      return;
+    }
+
+    cb();
+  })
+}
+
+function startServer (): void {
+  const app = express();
+  const server = http.createServer(app);
+
+  const wss = new WebSocketServer({ server });
+  wss.on('connection', (ws: WebSocket) => {
+    // constructs a websocket stream that knows how to deal with JS objects
+    const stream = new WebSocketJSONStream(ws);
+    backend.listen(stream); // let sharedb listen to this json stream
+  });
+
+  server.listen(8080, () => console.log('Server started.'));
+};
+
+createDoc(startServer);
