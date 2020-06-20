358758301bb3a6c6a64e1e6892636b8f4a5b2586
Jing Yen Loh <lohjingyen@gmail.com>
2020-06-21 01:24:57 +0800

    fix ShareDb db initialization

--- src/share.ts
+++ src/share.ts
@@ -1,10 +1,9 @@
 import ShareDb from 'sharedb';
 
 const share = new ShareDb({
-  db: ShareDb.MemoryDB,
+  db: new ShareDb.MemoryDB(),
 });
 
 export const connection = share.connect();
 
 export default share;
-
