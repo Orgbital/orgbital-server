02ca00dd357bb136facb8f6a402287f01e0b6b7c
Jing Yen Loh <lohjingyen@gmail.com>
2020-06-21 01:25:42 +0800

    Use morgan

--- src/app.ts
+++ src/app.ts
@@ -2,14 +2,16 @@ import { config as configEnv } from 'dotenv';
 import express from 'express';
 import bodyParser from 'body-parser';
 import cors from 'cors';
+import morgan from 'morgan';
 
 import { documentRouter } from './routes/document';
 
 configEnv();
 
 const app = express();
-app.use(bodyParser.urlencoded());
+app.use(bodyParser.json());
 app.use(cors());
+app.use(morgan('tiny'));
 
 app.get('/', (_, res) => res.sendStatus(200));
 // Despite the naming inconsistency, a collection is a group of documents
